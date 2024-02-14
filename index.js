5a60a96625646836f2b4488cbb69ff29d5b31156
codewithmahadihasan <codewithmahadihasan@gmail.com>
2024-02-15 02:29:55 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 7000;
+const cors = require('cors');
+// const AuthRouter = require('./Modules/Auth/AuthRouter');
+
+
+app.use(cors());
+app.use(express.json());
+
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+
+// API version 1 routes
+app.get('/api/v1/', (req, res) => {
+    res.send('Welcome to Sea Properties LTD API version 1!');
+});
+
+
+// app.use('/api/v1/users', UserRouter);
+
+
+
+// Error Handling Middleware
+app.use((req, res, next) => {
+    res.status(404).json({
+        success: false,
+        message: 'Not Found the API',
+        errorMessages: [
+            {
+                path: req.originalUrl,
+                message: 'API Not Found',
+            },
+        ],
+    });
+    next();
+});
+
+app.use((err, req, res, next) => {
+    if (err instanceof Error) {
+        const message = err.message;
+        const errorMessages = message
+            ? [
+                {
+                    path: '',
+                    message,
+                },
+            ]
+            : [];
+
+        console.error('Global Error Handler: ', err);
+
+        res.status(500).json({
+            status: false,
+            message: 'An error occurred',
+            errors: errorMessages,
+        });
+    } else {
+        next(err);
+    }
+});
+
+
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
