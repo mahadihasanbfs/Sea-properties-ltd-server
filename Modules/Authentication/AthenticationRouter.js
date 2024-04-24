f806b2b5ba2a5286811631aecf61d2458e621539
sarwar-asik <sarwarasik@gmail.com>
2024-04-24 23:29:29 +0600

    auth: password hashed included

--- Modules/Authentication/AthenticationRouter.js
+++ Modules/Authentication/AthenticationRouter.js
@@ -1,12 +1,9 @@
-
 const express = require("express");
 const router = express.Router();
 //functions for creating routes and importing here.
-const { add_user, get_user } = require("./AuthenticationModule")
+const { add_user, get_user } = require("./AuthenticationModule");
 
-
-router.get('/sign-in', get_user)
-router.post('/sign-up', add_user)
-
+router.get("/sign-in", get_user);
+router.post("/sign-up", add_user);
 
 module.exports = router;
