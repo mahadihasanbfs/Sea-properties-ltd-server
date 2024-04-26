2f05e6779283634865add89b72a6ce494aa08e81
sarwar-asik <sarwarasik@gmail.com>
2024-04-26 17:18:19 +0600

    contact: api created for contact

--- Modules/Admin/Contact.js
+++ Modules/Admin/Contact.js
@@ -1,63 +1,102 @@
 const { contact_collection } = require("../../Collections/admin_collection");
-const { ObjectId } = require('mongodb');
+const { ObjectId } = require("mongodb");
 
 const get_all_contacts = async (req, res, next) => {
-    try {
-        const result = await contact_collection.find({}).toArray();
+  try {
+    const result = await contact_collection.find({}).toArray();
 
-        res.send({
-            status: true,
-            data: result
-        });
-    } catch (err) {
-        res.status(500).send({
-            status: false,
-            message: "Failed to fetch contacts"
-        });
-    }
+    res.send({
+      status: true,
+      data: result,
+    });
+  } catch (err) {
+    res.status(500).send({
+      status: false,
+      message: "Failed to fetch contacts",
+    });
+  }
 };
 
 const delete_contact_by_id = async (req, res, next) => {
-    const id = req.query.contact_id;
+  const id = req.query.contact_id;
 
-    try {
-        const result = await contact_collection.deleteOne({ _id: new ObjectId(id) });
+  try {
+    const result = await contact_collection.deleteOne({
+      _id: new ObjectId(id),
+    });
 
-        if (result.deletedCount > 0) {
-            res.send({
-                status: true,
-                message: "Contact deleted successfully"
-            });
-        } else {
-            res.status(404).send({
-                status: false,
-                message: "Contact not found"
-            });
-        }
-    } catch (err) {
-        res.status(500).send({
-            status: false,
-            message: "Failed to delete contact"
-        });
+    if (result.deletedCount > 0) {
+      res.send({
+        status: true,
+        message: "Contact deleted successfully",
+      });
+    } else {
+      res.status(404).send({
+        status: false,
+        message: "Contact not found",
+      });
     }
+  } catch (err) {
+    res.status(500).send({
+      status: false,
+      message: "Failed to delete contact",
+    });
+  }
+};
+const update_contact_by_id = async (req, res, next) => {
+  const id = req.query.contact_id;
+  const data = req.body.data;
+  try {
+    const result = await contact_collection.updateOne(
+      { _id: id },
+      {
+        $set: data,
+      },
+      {
+        upsert: true,
+      }
+    );
+
+    if (result.deletedCount > 0) {
+      res.send({
+        status: true,
+        message: "Contact Updated successfully",
+      });
+    } else {
+      res.status(404).send({
+        status: false,
+        message: "Contact not found",
+      });
+    }
+  } catch (err) {
+    res.status(500).send({
+      status: false,
+      message: "Failed to delete contact",
+    });
+  }
 };
 
 const create_contact = async (req, res, next) => {
-    const contactData = req.body;
+  const contactData = req.body;
 
-    try {
-        const result = await contact_collection.insertOne(contactData);
-        res.send({
-            status: true,
-            message: "Contact created successfully",
-            data: result.ops[0] // Return the created contact data
-        });
-    } catch (err) {
-        res.status(500).send({
-            status: false,
-            message: "Failed to create contact"
-        });
-    }
+  try {
+    const result = await contact_collection.insertOne(contactData);
+    res.send({
+      status: true,
+      message: "Contact created successfully",
+      data: result,
+    });
+  } catch (err) {
+    res.status(500).send({
+      status: false,
+      message: "Failed to create contact",
+    });
+  }
 };
 
-module.exports = { get_all_contacts, delete_contact_by_id, create_contact };
+module.exports = {
+  get_all_contacts,
+  delete_contact_by_id,
+  create_contact,
+  update_contact_by_id,
+};
--- Router/AdminRouter.js
+++ Router/AdminRouter.js
@@ -1,71 +1,86 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
+const {
+  add_blog,
+  update_blog,
+  get_blog_by_id,
+  delete_blog,
+  get_all_blogs,
+} = require("../Modules/Admin/Blog");
 
-const { add_blog,
-    update_blog,
-    get_blog_by_id,
-    delete_blog,
-    get_all_blogs } = require('../Modules/Admin/Blog');
+const {
+  add_project,
+  update_project,
+  get_all_projects,
+  get_project_by_id,
+  delete_project,
+} = require("../Modules/Admin/Porject");
 
-const { add_project,
-    update_project,
-    get_all_projects,
-    get_project_by_id,
-    delete_project } = require('../Modules/Admin/Porject');
+const {
+  add_installment,
+  update_installment,
+  get_all_installments,
+  get_installment_by_id,
+  delete_installment,
+} = require("../Modules/Admin/Installment");
 
-const { add_installment,
-    update_installment,
-    get_all_installments,
-    get_installment_by_id,
-    delete_installment } = require('../Modules/Admin/Installment');
+const {
+  get_all_newsletters,
+  delete_newsletter_by_id,
+  create_newsletter,
+} = require("../Modules/Admin/news_letter");
 
-const { get_all_newsletters,
-    delete_newsletter_by_id,
-    create_newsletter } = require('../Modules/Admin/news_letter');
+const {
+  add_booking,
+  delete_booking,
+  get_booking_by_id,
+  get_all_bookings,
+  update_booking,
+} = require("../Modules/Admin/Booking");
+const {
+  get_all_contacts,
+  delete_contact_by_id,
+} = require("../Modules/Admin/Contact");
 
-const { add_booking,
-    delete_booking,
-    get_booking_by_id,
-    get_all_bookings,
-    update_booking } = require('../Modules/Admin/Booking');
+//blogs router:
 
+router.post("/blog/add", add_blog);
+router.put("/blog/update", update_blog);
+router.get("/blog/blogs", get_all_blogs);
+router.get("/blog/get-blog", get_blog_by_id);
+router.delete("/blog/delete", delete_blog);
 
-
-//blogs router: 
-
-router.post('/blog/add', add_blog);
-router.put('/blog/update', update_blog);
-router.get('/blog/blogs', get_all_blogs)
-router.get('/blog/get-blog', get_blog_by_id);
-router.delete('/blog/delete', delete_blog);
-
-// project router  
-router.post('/project/add', add_project)
-router.put('/project/update', update_project);
-router.get('/project/projects', get_all_projects)
-router.get('/project/get-project', get_project_by_id);
-router.delete('/project/delete', delete_project);
-
+// project router
+router.post("/project/add", add_project);
+router.put("/project/update", update_project);
+router.get("/project/projects", get_all_projects);
+router.get("/project/get-project", get_project_by_id);
+router.delete("/project/delete", delete_project);
 
 //installment router
-router.post('/installment/add', add_installment)
-router.put('/installment/update', update_installment);
-router.get('/installment/installments', get_all_installments)
-router.get('/installment/get-installment', get_installment_by_id);
-router.delete('/installment/delete', delete_installment);
+router.post("/installment/add", add_installment);
+router.put("/installment/update", update_installment);
+router.get("/installment/installments", get_all_installments);
+router.get("/installment/get-installment", get_installment_by_id);
+router.delete("/installment/delete", delete_installment);
 
-//news letter 
-router.get('/news-letter/get', get_all_newsletters)
-router.delete('/news-letter/delete-by-id', delete_newsletter_by_id)
-router.post('/news-letter/add', create_newsletter)
+//news letter
+router.get("/news-letter/get", get_all_newsletters);
+router.delete("/news-letter/delete-by-id", delete_newsletter_by_id);
+router.post("/news-letter/add", create_newsletter);
 
+//booking router
+router.post("/booking/add", add_booking);
+router.put("/booking/update", update_booking);
+router.get("/booking/bookings", get_all_bookings);
+router.get("/booking/get-booking", get_booking_by_id);
+router.delete("/booking/delete", delete_booking);
 
-//booking router 
-router.post('/booking/add', add_booking)
-router.put('/booking/update', update_booking);
-router.get('/booking/bookings', get_all_bookings)
-router.get('/booking/get-booking', get_booking_by_id);
-router.delete('/booking/delete', delete_booking);
+///!  contact
+
+router.get("/contacts", get_all_contacts);
+// router.get("/contact/get-contact", get_contact_by_id);
+router.delete("/contact/delete", delete_contact_by_id);
 
 module.exports = router;
