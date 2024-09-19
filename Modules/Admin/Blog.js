b4c61c328002a2dc85a76b858ecf86f7768e7365
codewithmahadihasan <73072248+codewithmahadihasan@users.noreply.github.com>
2024-09-19 15:09:50 +0600

    update code

--- Modules/Admin/Blog.js
+++ Modules/Admin/Blog.js
@@ -2,117 +2,119 @@ const { ObjectId } = require('mongodb');
 const { blog_collection } = require("../../Collections/admin_collection");
 
 const add_blog = async (req, res, next) => {
-    const body = req.body;
-   
-    try {
-        const result = await blog_collection.insertOne(body);
-        res.send({
-            status: true,
-            message: "Your blog uploaded successfully"
-        });
-    } catch (err) {
+      const body = req.body;
+      body.sku = body.title.toLowerCase().replace(/\s+/g, '_');
 
-        res.status(500).send({
-            status: false,
-            message: "Failed to upload blog"
-        });
-    }
+
+      try {
+            const result = await blog_collection.insertOne(body);
+            res.send({
+                  status: true,
+                  message: "Your blog uploaded successfully"
+            });
+      } catch (err) {
+
+            res.status(500).send({
+                  status: false,
+                  message: "Failed to upload blog"
+            });
+      }
 };
 
 const update_blog = async (req, res, next) => {
-    const id = req.query.blog_id;
-    const update = req.body;
-    console.log(id);
+      const id = req.query.blog_id;
+      const update = req.body;
+      console.log(id);
 
-    try {
-        const result = await blog_collection.updateOne({ _id: new ObjectId(id) }, { $set: { ...update } });
-        if (result.modifiedCount > 0) {
-            res.send({
-                status: true,
-                message: "Blog updated successfully"
+      try {
+            const result = await blog_collection.updateOne({ _id: new ObjectId(id) }, { $set: { ...update } });
+            if (result.modifiedCount > 0) {
+                  res.send({
+                        status: true,
+                        message: "Blog updated successfully"
+                  });
+            } else {
+                  res.status(404).send({
+                        status: false,
+                        message: "Blog not found"
+                  });
+            }
+      } catch (err) {
+            res.status(500).send({
+                  status: false,
+                  message: "Failed to update blog"
             });
-        } else {
-            res.status(404).send({
-                status: false,
-                message: "Blog not found"
-            });
-        }
-    } catch (err) {
-        res.status(500).send({
-            status: false,
-            message: "Failed to update blog"
-        });
-    }
+      }
 };
 
 const get_blog_by_id = async (req, res, next) => {
-    const id = decodeURIComponent(req.query.blog_id);
-    console.log(id);
-    try {
-        const pipeline = [
-            { $match: { name: id } }
-        ];
+      const id = decodeURIComponent(req.query.blog_id);
+      console.log(id);
+      try {
+            const pipeline = [
+                  { $match: { name: id } }
+            ];
 
-        const result = await blog_collection.aggregate(pipeline).toArray();
+            const result = await blog_collection.aggregate(pipeline).toArray();
 
-        if (result.length > 0) {
-            res.send({
-                status: true,
-                data: result[0]
+            if (result.length > 0) {
+                  res.send({
+                        status: true,
+                        data: result[0]
+                  });
+            } else {
+                  res.status(404).send({
+                        status: false,
+                        message: "Blog not found"
+                  });
+            }
+      } catch (err) {
+            res.status(500).send({
+                  status: false,
+                  message: "Failed to fetch blog"
             });
-        } else {
-            res.status(404).send({
-                status: false,
-                message: "Blog not found"
-            });
-        }
-    } catch (err) {
-        res.status(500).send({
-            status: false,
-            message: "Failed to fetch blog"
-        });
-    }
+      }
 };
 
 const delete_blog = async (req, res, next) => {
-    const id = req.query.blog_id;
+      const id = req.query.blog_id;
 
-    try {
-        const result = await blog_collection.deleteOne({ _id: new ObjectId(id) });
+      try {
+            const result = await blog_collection.deleteOne({ _id: new ObjectId(id) });
 
-        if (result.deletedCount > 0) {
-            res.send({
-                status: true,
-                message: "Blog deleted successfully"
+            if (result.deletedCount > 0) {
+                  res.send({
+                        status: true,
+                        message: "Blog deleted successfully"
+                  });
+            } else {
+                  res.status(404).send({
+                        status: false,
+                        message: "Blog not found"
+                  });
+            }
+      } catch (err) {
+            res.status(500).send({
+                  status: false,
+                  message: "Failed to delete blog"
             });
-        } else {
-            res.status(404).send({
-                status: false,
-                message: "Blog not found"
-            });
-        }
-    } catch (err) {
-        res.status(500).send({
-            status: false,
-            message: "Failed to delete blog"
-        });
-    }
+      }
 };
 
 const get_all_blogs = async (req, res, next) => {
-    try {
-        const result = await blog_collection.find({}).toArray();
+      try {
+            const result = await blog_collection.find({}).toArray();
 
-        res.send({
-            status: true,
-            data: result
-        });
-    } catch (err) {
-        res.status(500).send({
-            status: false,
-            message: "Failed to fetch blogs"
-        });
-    }
+            res.send({
+                  status: true,
+                  data: result
+            });
+      } catch (err) {
+            res.status(500).send({
+                  status: false,
+                  message: "Failed to fetch blogs"
+            });
+      }
 };
 
 
